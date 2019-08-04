e312973770c4741e0493ebc270d3b185aefcc10b
Danilo Woznica <danilowoz@gmail.com>
2019-08-04 20:56:42 +0100

    refactor(css func):

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import styled, { css } from "./styled";
 
 function App() {
   const [color, setColor] = useState("red");
+  const [active, setActive] = useState(false);
+
+  const SECTION_WIDTH = 400;
 
   const Section = styled.section`
-    color: red;
+    width: ${SECTION_WIDTH}px;
   `;
 
   const Strong = styled.strong`
@@ -16,29 +19,25 @@ function App() {
   const Text = styled.p`
     color: ${props => props.color};
     font-size: 16px;
-
-    ${() =>
-      css`
-        font-weight: bold;
-      `}
-
     text-align: center;
+
+    ${({ active }) =>
+      active &&
+      css`
+        text-decoration: underline;
+        text-transform: ${({ color }) =>
+          color === "red" ? "uppercase" : "none"};
+      `}
 
     strong,
     span {
-      color: orange;
+      color: #000;
       font-weight: normal;
     }
 
     ${Strong} {
-      color: black;
-
-      ${Strong} {
-        color: black;
-      }
+      font-size: 20px;
     }
-
-    font-size: 32px;
   `;
 
   const Button = styled.button`
@@ -48,8 +47,9 @@ function App() {
   return (
     <div>
       <Section id="foo">foo</Section>
-      <Button onClick={() => setColor("blue")}>click me</Button>
-      <Text as="h1" className="my-class" color={color}>
+      <Button onClick={() => setColor("blue")}>update color</Button>
+      <Button onClick={() => setActive(true)}>set active</Button>
+      <Text as="h1" className="my-class" color={color} active={active}>
         Texto
         <Strong> strong</Strong>
         <span> span</span>
